18affc2e35bc4353cf3975c630d69d5eeb00e01b
Nishu Tyagi <tyaginishu7@gmail.com>
2021-07-23 21:42:24 +0530

    Added:Welcome Message

---
+++ EmpWage.java
@@ -0,0 +1,7 @@
+import javax.lang.model.util.ElementScanner14;
+
+public class EmpWage {
+    public static void main (String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Problem");   
+    }
+}
