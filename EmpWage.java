48bcf6242d3625855e383283126f8d7591b4eafc
Nishu Tyagi <tyaginishu7@gmail.com>
2021-07-23 21:54:48 +0530

    Added:Employee Attendance

--- EmpWage.java
+++ EmpWage.java
@@ -2,6 +2,14 @@ import javax.lang.model.util.ElementScanner14;
 
 public class EmpWage {
     public static void main (String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Problem");   
+        System.out.println("Welcome to Employee Wage Computation Problem");
+        int Full_Time = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == Full_Time) {
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
