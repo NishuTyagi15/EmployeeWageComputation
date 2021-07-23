c9bbda150b81eb4129d7b8fd56c4000d216cb1db
Nishu Tyagi <tyaginishu7@gmail.com>
2021-07-23 22:28:34 +0530

    Added:Employee Wage

--- EmpWage.java
+++ EmpWage.java
@@ -4,12 +4,16 @@ public class EmpWage {
     public static void main (String[] args) {
         System.out.println("Welcome to Employee Wage Computation Problem");
         int Full_Time = 1;
+        int Emp_Rate_Per_Hour = 20;
+
+        int empHrs = 0;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == Full_Time) {
-            System.out.println("Employee is Present");
-        }
-        else {
-            System.out.println("Employee is Absent");
-        }
+        if (empCheck == Full_Time) 
+            empHrs = 8;
+        else 
+            empHrs = 0;
+        empWage = empHrs * Emp_Rate_Per_Hour;
+        System.out.println("Employee Wage is : " +empWage);
     }
 }
