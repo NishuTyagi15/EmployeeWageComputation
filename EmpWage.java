22e2c1771f9835a3917819007352ca893b343e88
Nishu Tyagi <tyaginishu7@gmail.com>
2021-07-24 10:36:07 +0530

    Refactored:Modified Using Switch Case Statement

--- EmpWage.java
+++ EmpWage.java
@@ -1,19 +1,29 @@
 import javax.lang.model.util.ElementScanner14;
 
+//Constants
 public class EmpWage {
+    public static final int Part_Time = 1;
+    public static final int Full_Time = 2;
+    public static final int Emp_Rate_Per_Hour = 20;
     public static void main (String[] args) {
         System.out.println("Welcome to Employee Wage Computation Problem");
-        int Part_Time = 1;
-        int Full_Time = 2;
-        int Emp_Rate_Per_Hour = 20;
 
+        //Variables
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == Full_Time) 
-            empHrs = 8;
-        else if (empCheck == Part_Time)
-            empHrs = 4;
+
+        //Computation
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case Full_Time :
+                empHrs = 8;
+                break;
+            case Part_Time :
+                empHrs = 4;
+                break;
+            default :
+                empHrs = 0;
+        }
         empWage = empHrs * Emp_Rate_Per_Hour;
         System.out.println("Employee Wage is : " +empWage);
     }
