c1f49871048dcc597c99f53f6cb75793fedec39e
Nishu Tyagi <tyaginishu7@gmail.com>
2021-07-24 10:00:16 +0530

    Refactored:Added Part Time Employee Wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,16 +3,17 @@ import javax.lang.model.util.ElementScanner14;
 public class EmpWage {
     public static void main (String[] args) {
         System.out.println("Welcome to Employee Wage Computation Problem");
-        int Full_Time = 1;
+        int Part_Time = 1;
+        int Full_Time = 2;
         int Emp_Rate_Per_Hour = 20;
 
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == Full_Time) 
             empHrs = 8;
-        else 
-            empHrs = 0;
+        else if (empCheck == Part_Time)
+            empHrs = 4;
         empWage = empHrs * Emp_Rate_Per_Hour;
         System.out.println("Employee Wage is : " +empWage);
     }
