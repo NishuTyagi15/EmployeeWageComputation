850bf301a70ac043695cabc97f482c7fee14d208
Nishu Tyagi <tyaginishu7@gmail.com>
2021-07-27 19:02:53 +0530

    Refactored:Modify the Employee Wage Computation

--- EmpWage.java
+++ EmpWage.java
@@ -1,12 +1,12 @@
-import javax.lang.model.util.ElementScanner14;
+import java.lang.Math;
 
 //Constants
 public class EmpWage {
-    public static final int Part_Time = 1;
-    public static final int Full_Time = 2;
-    public static final int Emp_Rate_Per_Hour = 20;
-    public static final int Working_Days = 20;
-    public static final int Hrs_In_Month = 100;
+    public static final int PARTTIME = 1;
+    public static final int FULLTIME = 2;
+    public static final int EMPRATEPERHOUR = 20;
+    public static final int WORKINGDAYS = 20;
+    public static final int HRSINMONTH = 100;
     public static void main (String[] args) {
         System.out.println("Welcome to Employee Wage Computation Problem");
 
@@ -14,28 +14,28 @@ public class EmpWage {
         int totalEmpWage = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
         //Computation
-        while (totalEmpHrs <= Hrs_In_Month &&
-               totalWorkingDays < Working_Days) {
+        while (totalEmpHrs <= HRSINMONTH &&
+               totalWorkingDays < WORKINGDAYS) {
             int empHrs = 0;
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
-                case Full_Time :
+                case FULLTIME :
                     empHrs = 8;
                     break;
-                case Part_Time :
+                case PARTTIME :
                     empHrs = 4;
                     break;
                 default :
                     empHrs = 0;
             }
             totalEmpHrs += empHrs;
-            int empWage = empHrs * Emp_Rate_Per_Hour;
+            int empWage = empHrs * EMPRATEPERHOUR;
             totalEmpWage += empWage;
             System.out.println("Employee Wage is : " +empWage);
         }
         System.out.println("Total Employee Wage is : " +totalEmpWage);
         System.out.println("Total Working Days : " + totalWorkingDays + 
-            " TotalEmployee Hours : " + totalEmpHrs);
+           " TotalEmployee Hours : " + totalEmpHrs);
     }
 }
